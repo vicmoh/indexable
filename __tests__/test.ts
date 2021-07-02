5f6034a584b5aaac5ed3e2e3bd6120bd49c5878b
Vicky Mohammad <vimoh1213@gmail.com>
2021-07-01 22:16:10 -0400

    [AUTO]

--- __tests__/test.ts
+++ __tests__/test.ts
@@ -1,26 +1,22 @@
 import { dateIndexer, datePathefier, normalize } from "../src/date-indexer";
-import { Epoch } from "../src/epoch";
 import { Indexable } from "../src/indexable";
 
 test("Jest test", () => {
   expect(true).toBe(true);
 });
 
-test("Milliseconds epoch test.", () => {
-  expect(Epoch.oneMinInMilEpoch() === 60000).toBe(true);
-  expect(Epoch.oneDay() === 86400000).toBe(true);
-
+test("datePathefier() should create the correct format.", () => {
   const date = new Date(1625109749836).getTime();
   expect(datePathefier(date) === "2021-7-1").toBe(true);
 });
 
-test("Normalize milliseconds test.", () => {
+test("normalise() should normalize the time to the current day.", () => {
   const test = 1625109749836;
   const ans = 1625109700000;
   expect(normalize(test.toString()) === ans.toString()).toBe(true);
 });
 
-test("Date indexer test", () => {
+test("dateIndexer() should create the correct dates of the index list", () => {
   const test = 1625109749836;
   const ans = [
     1625109700000, //
@@ -43,6 +39,6 @@ test("Date indexer test", () => {
   expect(res).toBe(true);
 });
 
-test("Indexable test.", () => {
+test("Log output test.", () => {
   console.log(new Indexable().dates(Date.now()));
 });
