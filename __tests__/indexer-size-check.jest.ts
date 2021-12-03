5c2f9ca3c3a271384d82a4b32c664d7b6ec46300
Vicky Mohammad <vimoh1213@gmail.com>
2021-12-03 03:18:46 -0500

    update test size for 32 char len

--- __tests__/indexer-size-check.jest.ts
+++ __tests__/indexer-size-check.jest.ts
@@ -3,20 +3,31 @@ import { TextIndexer } from "../src/text-indexer";
 const debug = false;
 const ndx = new TextIndexer();
 
+function indexSize(para: string): number {
+  const res = ndx.indexSubText(para);
+  if (debug) console.log(res);
+  let size = 0;
+  for (const sub of res) {
+    size += sub.length;
+  }
+  return size;
+}
+
 describe("Checking the size of the index.", () => {
   test("512 index string should be resoanable byte size.", async () => {
     const str =
       "bkppuruaiutqnoagaamstccsuccjqissduykavzrdjpovhuywgjhujacjceyvakqvbcauccezwerqcdatauyedxxewchywqryrylsipfvoerfsxfsmpivrrsuyjvrqkjklurhvokfzdbfmzaaxchvueejtyozdwgucsprzhrtlppbfwawntuzfperkxckdfmgaeuqeqlxrkfqsaerojirciggyhsqquoglybwoucuhypbjquigtgwydwhdetsmitlzvtuwyzlpxtffmqhudrhgomtkxisrabfllxcunqwfjzbceidauvhauldidjgcrixqkxvnaxlwvyavkpqhcvjarsgchorkpfskhftulmawagnsokkhbtjnxlpuwjumgzpyubhivdtlydaoxdpcighaqrsowauqfxtfckewiseujqwdenydaletltwqkbfazuscpavtbjlqzgtbooatbobbkfmyfkhfmaizasmkgcpaxwikyghhcxpiznmqebehsw";
+    const str32 = "xsaysbmmlqxwzdkrktgfkuyptycybhzz";
 
-    const res = ndx.indexSubText(str);
-    if (debug) console.log(res);
-    let size = 0;
-    for (const sub of res) {
-      size += sub.length;
-    }
+    const mb20 = 22000000; // 22mb
+    const mb003 = 30000; // 0.03mb
+    const size512 = indexSize(str);
+    const size32 = indexSize(str32) * 4; // multiple by 4 since char is 4 bytes
 
-    const mb20 = 22000000;
-    if (debug) console.log("size: ", size);
-    expect(size).toBeGreaterThan(mb20);
+    if (debug) console.log("size 512: ", size512);
+    expect(size512).toBeGreaterThan(mb20);
+
+    if (debug) console.log("size 32: ", size32);
+    expect(size32).toBeLessThan(mb003);
   });
 });
